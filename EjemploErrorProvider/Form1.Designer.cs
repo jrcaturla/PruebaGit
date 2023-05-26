c160cc1893df9870c171825ced787943e41b6ea8
jrcaturla <jrcaturla@gmail.com>
2023-05-26 12:17:31 +0200

    Agregar archivos de proyecto.

---
+++ EjemploErrorProvider/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace EjemploErrorProvider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.epTxt = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtDato2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.epTxt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(54, 37);
+            this.txtDato.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(76, 20);
+            this.txtDato.TabIndex = 0;
+            this.txtDato.TextChanged += new System.EventHandler(this.txtDato_TextChanged);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(298, 37);
+            this.btnProcesar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(56, 19);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // epTxt
+            // 
+            this.epTxt.ContainerControl = this;
+            // 
+            // txtDato2
+            // 
+            this.txtDato2.Location = new System.Drawing.Point(54, 89);
+            this.txtDato2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDato2.Name = "txtDato2";
+            this.txtDato2.Size = new System.Drawing.Size(76, 20);
+            this.txtDato2.TabIndex = 2;
+            this.txtDato2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDato2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.txtDato2);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtDato);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Ejemplo ErrorProvider";
+            ((System.ComponentModel.ISupportInitialize)(this.epTxt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.ErrorProvider epTxt;
+        private System.Windows.Forms.TextBox txtDato2;
+    }
+}
+
---
+++ EjemploErrorProvider/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploErrorProvider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            bool error = false;
+            foreach(char caracter in txtDato.Text)
+            {
+                if (char.IsDigit(caracter))
+                {
+                    error = true;
+                    break;
+                }
+
+            }
+            //Verificamos por la condición de error
+
+            if (error)
+
+                epTxt.SetError(txtDato, "No se admiten números");
+
+            else
+                epTxt.Clear();
+        }
+
+        private void txtDato_TextChanged(object sender, EventArgs e)
+        {
+            bool error = false;
+            foreach (char caracter in txtDato.Text)
+            {
+                if (char.IsDigit(caracter))
+                {
+                    error = true;
+                    break;
+                }
+
+            }
+            //verificamos por la condición de error
+
+            if (error)
+
+                epTxt.SetError(txtDato, "no se admiten números");
+
+            else
+                epTxt.Clear();
+        }
+
+        private void txtDato2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar) || Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false; //para indicar que no se debe tener en cuenta la pulsación de dicha tecla, esto en .NET se hace asignando un valor verdadero a la propiedad Handled del objeto e:
+                epTxt.Clear();
+            }
+
+            else
+            {
+                e.Handled = true;
+
+                epTxt.SetError(txtDato2, "Sólo se admiten números");
+            }
+
+        }
+
+       
+    }
+ }
+
